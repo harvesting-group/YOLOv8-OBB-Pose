1aff08dd0281264342ebde2bfffa16e0e9865bc9
ZHU TX <37172431+Taylover13@users.noreply.github.com>
2025-09-08 17:09:48 +0800

    Delete models directory

--- models/yolo/__init__.py
+++
@@ -1,8 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from ultralytics.models.yolo import classify, detect, obb, pose, segment, world
-
-from .model import YOLO, YOLOWorld
-# from ultralytics.models.yolo.model import YOLO, YOLOWorld
-
-__all__ = "classify", "segment", "detect", "pose", "obb", "world", "YOLO", "YOLOWorld", "obb_pos"
--- models/yolo/obb_pos/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-from .predict import OBBPOSEPredictor
-from .train import OBBPOSETrainer
-from .val import OBBPOSEValidator
-
-__all__ = "OBBPOSEPredictor", "OBBPOSETrainer", "OBBPOSEValidator"
